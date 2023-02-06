4de1cd92cef16fb6888ca17a2395c0fc66256dd1
PythonFZ <fabian.zills@web.de>
2023-02-06 09:28:10 +0100

    add CP2k Node

---
+++ znlib/nodes/cp2k.py
@@ -0,0 +1,51 @@
+"""CP2K interface without ASE calculator.
+
+This interface is less restrictive than CP2K Single Point.
+"""
+import pathlib
+import subprocess
+
+import ase.io
+import yaml
+from cp2k_input_tools.generator import CP2KInputGenerator
+from zntrack import Node, dvc, meta, utils, zn
+
+
+class CP2KSinglePoint(Node):
+    """Node for running CP2K Single point calculations."""
+
+    cp2k_shell: str = meta.Text("cp2k_shell.ssmp")
+    cp2k_params = dvc.params("cp2k.yaml")
+
+    cp2k_directory: pathlib.Path = dvc.outs(utils.nwd / "cp2k")
+    input_files = dvc.deps()
+
+    atoms_file = dvc.deps()  # TODO allow both, atoms file and atoms object
+    index: int = zn.params(-1)
+
+    def run(self):
+        """ZnTrack run method."""
+        self.cp2k_directory.mkdir(exist_ok=True)
+        with open(self.cp2k_params, "r") as file:
+            cp2k_input_dict = yaml.safe_load(file)
+
+        atoms = list(ase.io.iread(self.atoms_file))
+        atoms = atoms[self.index]
+        # TODO assert that they do not exist
+        cp2k_input_dict["force_eval"]["subsys"]["topology"] = {
+            "COORD_FILE_FORMAT": "XYZ",
+            "COORD_FILE_NAME": "atoms.xyz",
+        }
+        cp2k_input_dict["force_eval"]["subsys"]["cell"] = {
+            "A": list(atoms.cell[0]),
+            "B": list(atoms.cell[1]),
+            "C": list(atoms.cell[2]),
+        }
+
+        cp2k_input_script = "\n".join(CP2KInputGenerator().line_iter(cp2k_input_dict))
+        with self.operating_directory(move_on=subprocess.CalledProcessError):
+            ase.io.write(self.cp2k_directory / "atoms.xyz", atoms)
+            input_file = self.cp2k_directory / "input.cp2k"
+            input_file.write_text(cp2k_input_script)
+
+            subprocess.run(self.cp2k_shell, cwd=self.cp2k_directory, check=True)
---
+++ znlib/static/__init__.py
@@ -0,0 +1,1 @@
+"""Static files that ship with znlib."""
