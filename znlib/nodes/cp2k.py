a07683ed9a7d2089b69194850b384b618901c4e9
PythonFZ <fabian.zills@web.de>
2023-02-06 09:51:57 +0100

    use shell = True

--- znlib/nodes/cp2k.py
+++ znlib/nodes/cp2k.py
@@ -70,7 +70,8 @@ class CP2KYaml(Node):
             input_file.write_text(cp2k_input_script)
 
             subprocess.run(
-                [self.cp2k_bin, "-in", "input.cp2k"],
+                f"{self.cp2k_bin} -in input.cp2k",
                 cwd=self.cp2k_directory,
                 check=True,
+                shell=True
             )
